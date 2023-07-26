e1c4cd6a3106243c90b841d6d61f811ccecdead8
Nabeen Parajuli <parajulinabeen15@gmail.com>
2023-07-26 13:10:52 +0545

    commented the save course api

--- src/main/java/com/example/vidhyarthi/controller/CourseController.java
+++ src/main/java/com/example/vidhyarthi/controller/CourseController.java
@@ -1,16 +1,10 @@
 package com.example.vidhyarthi.controller;
 
-import com.example.vidhyarthi.Request.CourseRequest;
-import com.example.vidhyarthi.dto.Studentdetails;
 import com.example.vidhyarthi.repository.CourseRepository;
 import com.example.vidhyarthi.service.CourseService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
@@ -20,18 +14,18 @@ public class CourseController {
     private CourseRepository courseRepository;
 
 
-    @PostMapping("/save")
-    public ResponseEntity saveCourse(@Valid @RequestBody CourseRequest course) {
-        return new ResponseEntity<>(courseService.saveCourse(course), HttpStatus.CREATED);
-    }
-
-    @GetMapping("/name/{name}")
-    public ResponseEntity<List<Studentdetails>> getStudentDetailsFromCourseName(@PathVariable("name") String name) {
-        return new ResponseEntity<>(courseService.getStudentDetailsByCourse(name), HttpStatus.FOUND);
-    }
-
-    @GetMapping("/year/{year}")
-    public ResponseEntity<List<Studentdetails>> getStudentDetailsByEnrolledYear(@PathVariable("year") String year) {
-        return new ResponseEntity<>(courseService.getStudentDetailsByEnrollmentDate(year), HttpStatus.FOUND);
-    }
+//    @PostMapping("/save")
+//    public ResponseEntity saveCourse(@Valid @RequestBody CourseRequest course) {
+//        return new ResponseEntity<>(courseService.saveCourse(course), HttpStatus.CREATED);
+//    }
+//
+//    @GetMapping("/name/{name}")
+//    public ResponseEntity<List<Studentdetails>> getStudentDetailsFromCourseName(@PathVariable("name") String name) {
+//        return new ResponseEntity<>(courseService.getStudentDetailsByCourse(name), HttpStatus.FOUND);
+//    }
+//
+//    @GetMapping("/year/{year}")
+//    public ResponseEntity<List<Studentdetails>> getStudentDetailsByEnrolledYear(@PathVariable("year") String year) {
+//        return new ResponseEntity<>(courseService.getStudentDetailsByEnrollmentDate(year), HttpStatus.FOUND);
+//    }
 }
